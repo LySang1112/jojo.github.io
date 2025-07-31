de100ea118299a1bc6ed97d00a2525b0bbab35e2
LySang1112 <kamadosangjiro@gmail.com>
2025-07-31 14:50:56 +0700

    Add files via upload

---
+++ Thicuoiki/src/main/java/com/mycompany/thicuoiki/Thicuoiki.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thicuoiki;
+
+/**
+ *
+ * @author PCH107_SV
+ */
+public class Thicuoiki {
+
+    public static void main(String[] args) {    
+        Fmain neo=new Fmain();
+        neo.show();
+    }
+}
